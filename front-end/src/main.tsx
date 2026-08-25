e829100f4bf2ea747d9781c40ece894329e096c3
Brunno Felipe Bezerra <brunnofdev@gmail.com>
2026-08-25 10:29:26 -0300

    feat: implement login and register pages

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import { AuthProvider } from './contexts/AuthContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <AuthProvider>
       <App />
-    </BrowserRouter>
+    </AuthProvider>
+  </BrowserRouter>
   </React.StrictMode>,
 )
